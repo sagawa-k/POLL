645f69b41f2b3ab7647cfb0a17dce8742f3fc0f8
sagawa-k <rain.chap.chap@gmail.com>
2021-06-22 22:45:21 +0900

    key再生成

---
+++ mysite/local_settings.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '4va#eb03*zdbe@=sgpy-m=gd6)g(abzc)2-64w!+s5)=1gkcln'
--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ob7rvvy9+1od6^5uxbqm9-f7$^j$qdnib4wm72@!ky9eiv@hqj'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
